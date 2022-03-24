1321053d7fe58076356ec498099c8d85ceffd53a
hugosenari <hugosenari@gmail.com>
2022-03-23 22:51:01 -0300

    feat(nimpkgs): remove lock

--- master/flake.nix
+++ master/flake.nix
@@ -7,19 +7,19 @@
   inputs.flakeNimbleLib.type  = "github";
   inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
   
-  inputs.src-shmk-master.flake = false;
-  inputs.src-shmk-master.ref   = "refs/heads/master";
-  inputs.src-shmk-master.owner = "thisNimAgo";
-  inputs.src-shmk-master.repo  = "mk";
-  inputs.src-shmk-master.type  = "gitlab";
+  inputs.src-mk-master.flake = false;
+  inputs.src-mk-master.ref   = "refs/heads/master";
+  inputs.src-mk-master.owner = "thisNimAgo";
+  inputs.src-mk-master.repo  = "mk";
+  inputs.src-mk-master.type  = "gitlab";
   
   outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
   let 
     lib  = flakeNimbleLib.lib;
-    args = ["self" "nixpkgs" "flakeNimbleLib" "src-shmk-master"];
+    args = ["self" "nixpkgs" "flakeNimbleLib" "src-mk-master"];
   in lib.mkRefOutput {
     inherit self nixpkgs ;
-    src  = deps."src-shmk-master";
+    src  = deps."src-mk-master";
     deps = builtins.removeAttrs deps args;
     meta = builtins.fromJSON (builtins.readFile ./meta.json);
   };
